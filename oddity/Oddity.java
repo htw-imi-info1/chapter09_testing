98114bc88e0c4c4f8f9ae64ee8083a9d25d4652b
B Kleinen <kleinen@infrastructure.de>
2014-01-14 01:18:32 +0100

    oddity

---
+++ oddity/Oddity.java
@@ -0,0 +1,5 @@
+public class Oddity {
+    public static boolean isOdd(int i) {
+        return (i % 2) == 1;
+    }
+}
---
+++ oddity/OddityTest.java
@@ -0,0 +1,13 @@
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Ignore;
+
+public class OddityTest
+{
+
+   @Test
+   public void isOdd(){
+       assertEquals(true,Oddity.isOdd(3));
+   }
+}

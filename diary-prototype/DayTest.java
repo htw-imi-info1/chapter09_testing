46f9fcb77951bed8e4119c4378b1995fd4209e1e
B Kleinen <kleinen@infrastructure.de>
2019-12-10 13:27:37 +0100

    added test fixtures

--- diary-prototype/DayTest.java
+++ diary-prototype/DayTest.java
@@ -1,5 +1,4 @@
 
-
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
@@ -35,22 +34,98 @@ public class DayTest
         day1 = new Day(1); // this is the sut - subject under test
         info2 = new Appointment("Info 1", 2);
         lab = new Appointment("Lab", 2);
-        day1.makeAppointment(9, info2);
-        day1.makeAppointment(12, lab);
         gdm = new Appointment("GDM", 2);
+
     }
 
-    
+    private void dayWith1hBreak(){
+        day1.makeAppointment(9, info2);
+        day1.makeAppointment(12, lab);
+    }
 
     @Test
     public void test2hSlotIsFound()
     {
         // given
         // day with 2h app on 9 and 12 hrs (setup)
+        dayWith1hBreak();
         // when
         int actualValue = day1.findSpace(gdm);
         // then
         assertEquals("first slot with 2h available should be found",14, actualValue);
     }
+
+    @Test
+    public void testMakeApp15()
+    {
+        // given
+        // day with 2h app on 9 and 12 hrs (setup)
+        dayWith1hBreak();
+        // when
+        boolean actual = day1.makeAppointment(15, gdm);
+        // then
+        assertEquals(true, actual);
+    }
+
+    @Test
+    public void testMakeApp16()
+    {
+        // given
+        // day with 2h app on 9 and 12 hrs (setup)
+        dayWith1hBreak();
+        // when
+        boolean actual = day1.makeAppointment(16, gdm);
+        // then
+        assertEquals(true, actual);
+    }
+
+    @Test
+    public void testMakeApp17()
+    {
+        // given
+        // day with 2h app on 9 and 12 hrs (setup)
+         dayWith1hBreak();
+       // when
+        boolean actual = day1.makeAppointment(17, gdm);
+        // then
+        assertEquals(false, actual);
+    }
+
+    @Test
+    public void testMakeApp18()
+    {
+        // given
+         dayWith1hBreak();
+       // day with 2h app on 9 and 12 hrs (setup)
+        // when
+        boolean actual = day1.makeAppointment(18, gdm);
+        // then
+        assertEquals(false, actual);
+    }
+
+    @Test
+    public void testMakeApp9()
+    {
+        // given
+       // empty day
+        // when
+        boolean actual = day1.makeAppointment(9, gdm);
+        // then
+        assertEquals(true, actual);
+    }
+
+    
+
+    @Test
+    public void testAppointmentWasActuallyScheduled()
+    {
+        // given empty day
+        // when 
+      day1.makeAppointment(14, info2);
+      // then appointment is in schedule
+        Appointment actual = day1.getAppointment(14);
+        assertEquals(info2, actual);
+    }
 }
+
 

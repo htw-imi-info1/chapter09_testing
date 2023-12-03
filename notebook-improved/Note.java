4872b02e611bfc97079058f86dda31185010615f
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-12-03 22:34:42 +0100

    notebook with Note class

--- notebook-improved/Note.java
+++ notebook-improved/Note.java
@@ -1,9 +1,29 @@
 
 public class Note
 {
-    public Note(String title, String text){
-        this.title = title; this.text = text;
-    }
     String title, text;
+
+    public Note(String title, String text){
+        this.title = title; 
+        this.text = text;
+    }
+
+    @Override
+    public String toString(){
+        return "# "+title+"\n"+text+"\n";
+    }
+
+    @Override
+    public boolean equals(Object other){
+        if (!(other instanceof Note))
+            return false;
+        Note otherNote = (Note)other;
+        return this.toString().equals(otherNote.toString());
+    }
+
+    @Override
+    public int hashCode(){
+        return toString().hashCode();
+    }
 
 }
